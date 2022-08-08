801b77ebc3bd2b22ac6111a1a2335b6a1b416da7
Nisarg Jaiswal <nisujaiswal4@gmail.com>
2022-08-08 12:45:16 +0530

    Models Created

---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    pic: {
+        type: String,
+        required: true,
+        default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userModel)
